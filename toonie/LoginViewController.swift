601a4515c6ecf1371bfa48792c591101563313dd
Angelina <angelina.zhai@mail.utoronto.ca>
2022-09-23 15:06:39 -0400

    Log-in and Sign-up pages are now working. Need to output error messages

--- toonie/LoginViewController.swift
+++ toonie/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
@@ -17,6 +18,7 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func loginTapped(_ sender: Any) {
+        validateFields()
     }
     
     @IBAction func createAccountTapped(_ sender: Any) {
@@ -24,6 +26,44 @@ class LoginViewController: UIViewController {
         let vc = storyboard.instantiateViewController(withIdentifier: "signUp")
         vc.modalPresentationStyle = .overFullScreen
         present(vc, animated: true)
+    }
+    
+    //check if fields have text
+    func validateFields(){
+        if email.text?.isEmpty == true {
+            print("No email text")
+            return
+        }
+        if pw.text?.isEmpty == true {
+            print("No password text")
+            return
+        }
+        login()
+    }
+    
+    //login
+    func login(){
+        Auth.auth().signIn(withEmail: email.text!, password: pw.text!) { [weak self] authResult, err in guard let strongSelf = self else {return}
+            guard let user = authResult?.user, err == nil else {
+                print("Error \(err?.localizedDescription)")
+                return
+            }
+            self?.checkUserInfo()
+        }
+    }
+    
+    
+    //check user input
+    func checkUserInfo(){
+        if Auth.auth().currentUser != nil{
+            //if passed authentication check
+            let storyboard = UIStoryboard(name: "Main", bundle:nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "mainHome")
+            print("Passed Authentication")
+            vc.modalPresentationStyle = .overFullScreen
+            present(vc, animated: true)
+        }
+      
     }
     
 }
--- toonie/SignupViewController.swift
+++ toonie/SignupViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseCore
 
 class SignupViewController: UIViewController {
 
@@ -16,10 +18,44 @@ class SignupViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    @IBAction func signUpTapped(_ sender: Any) {
+        if email.text?.isEmpty == true {
+            print("No text entered for email")
+            //alert add here
+            return
+        }
+        
+        if pw.text?.isEmpty == true {
+            print("No text entered for password")
+            //alert add here
+            return
+        }
+        signup()
+    }
+    
+    
     @IBAction func loginTapped(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Main", bundle:nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "login")
         vc.modalPresentationStyle = .overFullScreen
         present(vc, animated: true)
+    }
+    
+    func signup(){
+        Auth.auth().createUser(withEmail: email.text!, password: pw.text!){
+            (authResult, error) in
+            guard let user = authResult?.user, error == nil else {
+                print("Error \(error?.localizedDescription)")
+                return
+            }
+           
+            //if passed authentication check
+            let storyboard = UIStoryboard(name: "Main", bundle:nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "mainHome")
+            print("Passed Authentication")
+            vc.modalPresentationStyle = .overFullScreen
+            self.present(vc, animated: true)
+            
+        }
     }
 }
